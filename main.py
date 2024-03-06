0df59069232e11b25048c50ca5250987979ad982
david <automorphicnew@gmail.com>
2024-03-07 05:50:21 +0900

    fix

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 # https://python.langchain.com/docs/gfrom%20langchain_community.embeddings%20import%20OllamaEmbeddings%20embeddings%20=%20OllamaEmbeddings()
-
-
+import time
+start =time.time()
 from langchain_community.llms import Ollama
 llm = Ollama(model="llama2")
 
 from langchain_core.prompts import ChatPromptTemplate
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are world class technical documentation writer."),
-    ("user", "{input}")
-])
-
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system", "You are world class technical documentation writer."),
+#     ("user", "{input}")
+# ])
+#
 from langchain_core.output_parsers import StrOutputParser
 
-output_parser = StrOutputParser()
-
-chain = prompt | llm | output_parser
-
-chain.invoke({"input": "how can langsmith help with testing?"})
-
-
-
+# output_parser = StrOutputParser()
+#
+# chain = prompt | llm | output_parser
+#
+# chain.invoke({"input": "how can langsmith help with testing?"})
+#
+#
+#
 from langchain_community.embeddings import OllamaEmbeddings
 
 embeddings = OllamaEmbeddings()
-
-
+#
+#
 from langchain_community.document_loaders import WebBaseLoader
 loader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
 
 docs = loader.load()
-
-
+#
+#
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -40,7 +40,7 @@ documents = text_splitter.split_documents(docs)
 vector = FAISS.from_documents(documents, embeddings)
 
 
-chain.invoke({"input": "how can langsmith help with testing?"})
+# chain.invoke({"input": "how can langsmith help with testing?"})
 
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
@@ -61,6 +61,10 @@ retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
 response = retrieval_chain.invoke({"input": "how can langsmith help with testing?"})
 print(response["answer"])
+
+
+elapsed_time = time.time() -start
+print(f"elapsed_time :{elapsed_time}")
 
 # LangSmith offers several features that can help with testing:...
 
